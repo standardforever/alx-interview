9de3439bd5d2ec85e6fb224ca8f4091ade04d8f8
standardforver <standard.forver123@gmail.com>
2023-02-11 09:05:15 -0500

    interview

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Interview question"""
+
+def minOperation(n):
+    """find total num of operarion performed"""
+    if not isinstance(n, int) or n <= 1:
+        return 0
+    num = 1
+    count = 0
+    dup = 0
+    while (num < n):
+        if (n % num != 0):
+            num += dup
+            count += 1
+        else:
+            dup = num
+            num += dup
+            count += 2
+    if num == n:
+        return (count)
+    return 0
