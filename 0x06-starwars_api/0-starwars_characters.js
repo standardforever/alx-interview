b9e73180a07cea6f381af2947c3aa63e64697d15
standardforver <standard.forver123@gmail.com>
2023-03-09 21:48:50 -0500

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,25 @@
-
 #!/usr/bin/node
 const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-  request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (err, res, body) => {
-    if (err) console.log(err);
-    const json = JSON.parse(body);
-    const characters = json.characters.map(
-      (item) => new Promise((resolve, reject) => {
-        request(item, (err, res, body) => {
-          if (err) reject(err);
-          resolve(JSON.parse(body).name);
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
         });
       }));
 
-    Promise.all(characters)
-      .then((names) => console.log(names.join('\n')))
-      .catch((err) => console.log(err));
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
 }
